87b295335632a972f45e38a0719c5fda1612de58
Zeno Pelgrims <zenopelgrims@gmail.com>
2016-10-07 22:24:32 +0200

    Added cube primitive

--- ae/aiLightColorAttenuationTemplate.py
+++ ae/aiLightColorAttenuationTemplate.py
@@ -3,13 +3,45 @@ from mtoa.ui.ae.shaderTemplate import ShaderAETemplate
 
 class AEaiLightColorAttenuationTemplate(ShaderAETemplate):
     def setup(self):
+        self.beginScrollLayout()
         self.beginLayout('Color Attenuation', collapse=False)
+
+        self.addControl("aiMode", label="Decay / Blocker")
         self.addControl("aiColorStart", label="Start Color")
         self.addControl("aiColorEnd", label="End Color")
         self.addControl("aiInterpolationMethod", label="Interpolation Method [off = linear / on = hermite-cubic]")
         self.addControl("aiAttenuationStart", label="Attenuation Start")
         self.addControl("aiAttenuationEnd", label="Attenuation End")
 
+        self.addSeparator()
+
+        self.addControl("aiGeo", label="Sphere / Cube")
+        self.addControl("aiMath", label="Additive / Subtractive")
+
+        self.addSeparator()
+
+        self.addControl("aiLocatorLocation", label="Locator Location")
+        self.addControl("aiScale", label="Blocker Scale")
+        self.addControl("aiSphereRadius", label="Sphere Radius")
+
+        self.addSeparator()
+
+        self.addControl("aiColorAttenuation", label="Blocker Color Attenuation")
+        self.addControl("aiColorDensity", label="Blocker Color Density")
+
+        self.addSeparator()
+
+        self.addControl("aiRampX", label="Ramp X Axis")
+        self.addControl("aiFlipRampX", label="Flip X Axis")
+
+        self.addControl("aiRampY", label="Ramp Y Axis")
+        self.addControl("aiFlipRampY", label="Flip Y Axis")
+
+        self.addControl("aiRampZ", label="Ramp Z Axis")
+        self.addControl("aiFlipRampZ", label="Flip Z Axis")
+
+
         self.endLayout()
         pm.mel.AEdependNodeTemplate(self.nodeName)
         self.addExtraControls()
+        self.endScrollLayout()
--- src/lightcolorattenuation.cpp
+++ src/lightcolorattenuation.cpp
@@ -1,228 +1,255 @@
+// EXTENDED ARNOLD LIGHT FILTER
+
+// FEATURES:
+// COLOR ATTENUATION
+// MULTIPLE NEGATIVE BLOCKERS
+// MULTIPLE RAMPS
+
 #include <ai.h>
 #include <iostream>
 #include <string>
-#include <strings.h>
-#include <algorithm>
+#include <cstring>
 #include <vector>
-#include <iterator>
-#include <stdlib.h>
+
+
+AI_SHADER_NODE_EXPORT_METHODS(lightColorAttenuationMethods);
 
 
 enum lightColorAttenuationParams {
-   p_colorStart,
-   p_colorEnd,
-   p_interpolationMethod,
-   p_attenuationStart,
-   p_attenuationEnd,
-   p_locatorLocation,
-   p_negativeColor
+    p_mode,
+    p_colorStart,
+    p_colorEnd,
+    p_interpolationMethod,
+    p_attenuationStart,
+    p_attenuationEnd,
+    p_geo,
+    p_math,
+    p_locatorLocation,
+    p_scale,
+    p_sphereRadius,
+    p_colorAttenuation,
+    p_colorDensity,
+    p_rampX,
+    p_flipRampX,
+    p_rampY,
+    p_flipRampY,
+    p_rampZ,
+    p_flipRampZ
 };
 
 
 struct lightBlockers{
     std::vector<std::string> lightFilterNames;
     std::vector<float> locatorLocation;
-    std::vector<AtColor> color;
     std::vector<float> sphereRadius;
-} lb;
+} lightblockers;
 
-
-
-AI_SHADER_NODE_EXPORT_METHODS(lightColorAttenuationMethods);
 
 
 node_parameters {
-    AiParameterRGB("colorStart", 0.3, 1.0, 1.0);
-    AiParameterRGB("colorEnd", 1.0, 0.3, 0.6);
+    AiParameterBOOL("mode", true);
+    AiParameterRGB("colorStart", 1.0, 1.0, 1.0);
+    AiParameterRGB("colorEnd", 1.0, 1.0, 1.0);
     AiParameterBOOL("interpolationMethod", false);
-    AiParameterFLT("attenuationStart", 55.0);
-    AiParameterFLT("attenuationEnd", 105.0);
+    AiParameterFLT("attenuationStart", 0.001);
+    AiParameterFLT("attenuationEnd", 10.0);
+    AiParameterBOOL("geo", false);
+    AiParameterBOOL("math", false);
     AiParameterPNT("locatorLocation", 0.0, 0.0, 0.0);
-    AiParameterRGB("negativeColor", 0.0, 1.0, 1.0);
+    AiParameterVEC("scale", 1.0, 1.0, 1.0);
+    AiParameterFLT("sphereRadius", 1.0);
+    AiParameterRGB("colorAttenuation", 1.0, 1.0, 1.0);
+    AiParameterFLT("colorDensity", 1.0);
+    AiParameterFLT("rampX", 0.0);
+    AiParameterBOOL("flipRampX", false);
+    AiParameterFLT("rampY", 0.0);
+    AiParameterBOOL("flipRampY", false);
+    AiParameterFLT("rampZ", 0.0);
+    AiParameterBOOL("flipRampZ", false);
 }
 
-node_initialize {
-    lb.lightFilterNames.clear();
-    lb.locatorLocation.clear();
-}
+node_initialize {}
 
 
 node_update {
-    lb.lightFilterNames.clear();
-    lb.locatorLocation.clear();
+    lightblockers.lightFilterNames.clear();
+    lightblockers.locatorLocation.clear();
 }
 
 node_finish {
-    AiMsgInfo("vector size [%d]", lb.lightFilterNames.size());
-    for (int i = 0; i < lb.lightFilterNames.size(); i++){
-       AiMsgInfo("vector element [%d] = %s", i, lb.lightFilterNames[i].c_str());
-       AiMsgInfo("vector element [%d] = [%f, %f, %f]", i, lb.locatorLocation[i*3], lb.locatorLocation[i*3+1], lb.locatorLocation[i*3+2]);
+    AiMsgInfo("vector size [%lu]", lightblockers.lightFilterNames.size());
+    for (int i = 0; i < lightblockers.lightFilterNames.size(); i++){
+    AiMsgInfo("vector element [%d] = %s", i, lightblockers.lightFilterNames[i].c_str());
+    AiMsgInfo("vector element [%d] = [%f, %f, %f]", i, lightblockers.locatorLocation[i*3], lightblockers.locatorLocation[i*3+1], lightblockers.locatorLocation[i*3+2]);
     }
 
-
-    lb.lightFilterNames.clear();
-    lb.locatorLocation.clear();
+    lightblockers.lightFilterNames.clear();
+    lightblockers.locatorLocation.clear();
 }
 
 shader_evaluate{
-
     // get values
+    bool mode = AiShaderEvalParamBool(p_mode);
+
     AtColor colorStart = AiShaderEvalParamRGB(p_colorStart);
     AtColor colorEnd = AiShaderEvalParamRGB(p_colorEnd);
     bool interpolationMethod = AiShaderEvalParamBool(p_interpolationMethod);
     float attenuationStart = AiShaderEvalParamFlt(p_attenuationStart);
     float attenuationEnd = AiShaderEvalParamFlt(p_attenuationEnd);
+
+    bool geo = AiShaderEvalParamBool(p_geo);
+    bool math = AiShaderEvalParamBool(p_math);
+
     AtPoint locatorLocation = AiShaderEvalParamPnt(p_locatorLocation);
-    AtColor negativeColor = AiShaderEvalParamRGB(p_negativeColor);
+    AtVector scale = AiShaderEvalParamVec(p_scale);
+    float sphereRadius = AiShaderEvalParamFlt(p_sphereRadius);
+
+    AtColor colorAttenuation = AiShaderEvalParamRGB(p_colorAttenuation);
+    float colorDensity = AiShaderEvalParamFlt(p_colorDensity);
+
+    float rampX = AiShaderEvalParamFlt(p_rampX);
+    bool flipRampX = AiShaderEvalParamBool(p_flipRampX);
+
+    float rampY = AiShaderEvalParamFlt(p_rampY);
+    bool flipRampY = AiShaderEvalParamBool(p_flipRampY);
+
+    float rampZ = AiShaderEvalParamFlt(p_rampZ);
+    bool flipRampZ = AiShaderEvalParamBool(p_flipRampZ);
+
 
     std::string nodeName = std::string(AiNodeGetName(node));
 
-    bool decayBlockerSwitch = 1;
-    bool additiveSubtractiveSwitch = 0;
-    bool geometrySwitch = 1;
-
-    float colorDensity = 0.0;
-    float inverseColorDensity = (1.0 - colorDensity);
-
-    double sphereRadius = 15.0;
-    AtPoint cubeScale = {15.0, 15.0, 15.0};
-
-    float rampX = 0.2;
-    float rampY = 0.0;
-    float rampZ = 0.2;
-
-    bool flipRampX = true;
-    bool flipRampY = false;
-    bool flipRampZ = true;
+    //float sphereRamp = 0.3;
 
 
-    if (decayBlockerSwitch == 0){
+    // DECAY
+    if (!mode){
         if (sg->Ldist < attenuationStart){
             sg->Liu *= colorStart;
         } else if (sg->Ldist > attenuationEnd){
             sg->Liu *= colorEnd;
         } else {
-            // calculate how far the shading point is in respect to the distance between attenuation start and attenuation end
             double percentage = ((sg->Ldist - attenuationStart) / (attenuationEnd - attenuationStart));
             if (interpolationMethod == 0){
-                           sg->Liu *= AiColorLerp(percentage, colorStart, colorEnd);
+                sg->Liu *= AiColorLerp(percentage, colorStart, colorEnd);
             } else if (interpolationMethod == 1){
-                           sg->Liu *= AiColorHerp(percentage, colorStart, colorEnd);
+                sg->Liu *= AiColorHerp(percentage, colorStart, colorEnd);
             }
         }
     }
 
-    else if (decayBlockerSwitch == 1){
-        // ADDITIVE MODE
-        if (additiveSubtractiveSwitch == 0){
-            if (geometrySwitch == 0){
-               if (AiV3Dist(sg->P, locatorLocation) < sphereRadius){
-                              sg->Liu = AiColorLerp(inverseColorDensity, sg->Liu, sg->Liu * colorEnd);
-               }
-            } else {
-                if (ABS((sg->P.x - locatorLocation.x)) > ABS(cubeScale.x) ||
-                    ABS((sg->P.y - locatorLocation.y)) > ABS(cubeScale.y) ||
-                    ABS((sg->P.z - locatorLocation.z)) > ABS(cubeScale.z)){
-                                  // do nothing
+    // BLOCKER
+    else if (mode){
+        // ADDITIVE MATH
+        if (!math){
+            if (!geo){
+                if (AiV3Dist(sg->P, locatorLocation) < sphereRadius){
+                    //double sphereRampPercentage;
+                    //sphereRampPercentage = AiV3Dist(sg->P, locatorLocation) / sphereRamp;
+                    //if (sphereRampPercentage > 1.0){
+                    //            sphereRampPercentage = 1.0;
+                    //}
+                    //sg->Liu = AiColorLerp(sphereRampPercentage * colorDensity, sg->Liu, sg->Liu * colorAttenuation);
+                    sg->Liu = AiColorLerp(colorDensity, sg->Liu, sg->Liu * colorAttenuation);
+                }
                 } else {
-                  double rampxPercentage = 1.0;
-                  double rampyPercentage = 1.0;
-                  double rampzPercentage = 1.0;
+                    if (ABS((sg->P.x - locatorLocation.x)) > ABS(scale.x) ||
+                    ABS((sg->P.y - locatorLocation.y)) > ABS(scale.y) ||
+                    ABS((sg->P.z - locatorLocation.z)) > ABS(scale.z)){
+                    // do nothing
+                } else {
 
-                if (rampX > 0.0){
-                    if (!flipRampX){
-                        rampxPercentage = ((sg->P.x - (locatorLocation.x - cubeScale.x)) / (((locatorLocation.x + cubeScale.x) - (locatorLocation.x - cubeScale.x)) * rampX));
+                    double rampxPercentage = 1.0;
+                    double rampyPercentage = 1.0;
+                    double rampzPercentage = 1.0;
+
+                    if (rampX > 0.0){
+                        if (!flipRampX){
+                            rampxPercentage = ((sg->P.x - (locatorLocation.x - scale.x)) / (((locatorLocation.x + scale.x) - (locatorLocation.x - scale.x)) * rampX));
+                        } else {
+                            rampxPercentage = ABS(((sg->P.x - (locatorLocation.x + scale.x)) / (((locatorLocation.x + scale.x) - (locatorLocation.x - scale.x)) * rampX)));
+                        }
+
+                        if (rampxPercentage > 1.0){rampxPercentage = 1.0;}
+                    }
+
+                    if (rampY > 0.0){
+                        if (!flipRampY){
+                            rampyPercentage = ((sg->P.y - (locatorLocation.y - scale.y)) / (((locatorLocation.y + scale.y) - (locatorLocation.y - scale.y)) * rampY));
+                        } else {
+                            rampyPercentage = ABS(((sg->P.y - (locatorLocation.y + scale.y)) / (((locatorLocation.y + scale.y) - (locatorLocation.y - scale.y)) * rampY)));
+                        }
+
+                        if (rampyPercentage > 1.0){rampyPercentage = 1.0;}
+                    }
+
+                    if (rampZ > 0.0){
+                        if (!flipRampZ){
+                            rampzPercentage = ((sg->P.z - (locatorLocation.z - scale.z)) / (((locatorLocation.z + scale.z) - (locatorLocation.z - scale.z)) * rampZ));
+                        } else {
+                        rampzPercentage = ABS(((sg->P.z - (locatorLocation.z + scale.z)) / (((locatorLocation.z + scale.z) - (locatorLocation.z - scale.z)) * rampZ)));
+                        }
+
+                        if (rampzPercentage > 1.0){rampzPercentage = 1.0;}
+                    }
+
+                    if (rampX > 0.0 || rampY > 0.0 || rampZ > 0.0){
+                        sg->Liu = AiColorLerp(rampxPercentage * rampyPercentage * rampzPercentage * colorDensity, sg->Liu, sg->Liu * colorAttenuation);
                     } else {
-                        rampxPercentage = ((sg->P.x - (locatorLocation.x + cubeScale.x)) / (((locatorLocation.x + cubeScale.x) - (locatorLocation.x - cubeScale.x)) * rampX));
-                        rampxPercentage = ABS(rampxPercentage);
+                        sg->Liu = AiColorLerp(colorDensity, sg->Liu, sg->Liu * colorAttenuation);
                     }
-
-                    if (rampxPercentage > 1.0){
-                        rampxPercentage = 1.0;
-                    }
-                }
-
-                if (rampY > 0.0){
-                    if (!flipRampY){
-                        rampyPercentage = ((sg->P.y - (locatorLocation.y - cubeScale.y)) / (((locatorLocation.y + cubeScale.y) - (locatorLocation.y - cubeScale.y)) * rampY));
-                    } else {
-                        rampyPercentage = ((sg->P.y - (locatorLocation.y + cubeScale.y)) / (((locatorLocation.y + cubeScale.y) - (locatorLocation.y - cubeScale.y)) * rampY));
-                        rampyPercentage = ABS(rampyPercentage);
-                    }
-
-                    if (rampyPercentage > 1.0){
-                         rampyPercentage = 1.0;
-                    }
-                }
-
-                if (rampZ > 0.0){
-                    if (!flipRampZ){
-                         rampzPercentage = ((sg->P.z - (locatorLocation.z - cubeScale.z)) / (((locatorLocation.z + cubeScale.z) - (locatorLocation.z - cubeScale.z)) * rampZ));
-                    } else {
-                      rampzPercentage = ((sg->P.z - (locatorLocation.z + cubeScale.z)) / (((locatorLocation.z + cubeScale.z) - (locatorLocation.z - cubeScale.z)) * rampZ));
-                      rampzPercentage = ABS(rampzPercentage);
-                    }
-
-                    if (rampzPercentage > 1.0){
-                        rampzPercentage = 1.0;
-                    }
-                }
-
-                if (rampX > 0.0 || rampY > 0.0 || rampZ > 0.0){
-                    sg->Liu = AiColorLerp(rampxPercentage * rampyPercentage * rampzPercentage * inverseColorDensity, sg->Liu, sg->Liu * colorEnd);
-                }
 
                 }
 
             }
         }
 
-        // SUBTRACTIVE MODE
+        // SUBTRACTIVE MATH
         // sometimes more than 2 filters are picked up, not sure why... then it all goes phithongelong
-        else if (additiveSubtractiveSwitch == 1){
+        else if (math){
             bool inRadius = false;
             bool foundName = false;
-            if (lb.lightFilterNames.size() == 0){
-                lb.lightFilterNames.push_back(nodeName);
-                lb.locatorLocation.push_back(locatorLocation.x);
-                lb.locatorLocation.push_back(locatorLocation.y);
-                lb.locatorLocation.push_back(locatorLocation.z);
+            if (lightblockers.lightFilterNames.size() == 0){
+                lightblockers.lightFilterNames.push_back(nodeName);
+                lightblockers.locatorLocation.push_back(locatorLocation.x);
+                lightblockers.locatorLocation.push_back(locatorLocation.y);
+                lightblockers.locatorLocation.push_back(locatorLocation.z);
             } else {
-                for (int i = 0; i < lb.lightFilterNames.size(); i++){
-                    if (nodeName == lb.lightFilterNames[i]){
+                for (int i = 0; i < lightblockers.lightFilterNames.size(); i++){
+                    if (nodeName == lightblockers.lightFilterNames[i]){
                         foundName = true;
                     }
                 }
 
-                if (foundName == false){
-                    lb.lightFilterNames.push_back(nodeName);
-                    lb.locatorLocation.push_back(locatorLocation.x);
-                    lb.locatorLocation.push_back(locatorLocation.y);
-                    lb.locatorLocation.push_back(locatorLocation.z);
+                if (!foundName){
+                    lightblockers.lightFilterNames.push_back(nodeName);
+                    lightblockers.locatorLocation.push_back(locatorLocation.x);
+                    lightblockers.locatorLocation.push_back(locatorLocation.y);
+                    lightblockers.locatorLocation.push_back(locatorLocation.z);
                 } else {
-                    for(int i = 0; i < lb.lightFilterNames.size(); i++){
-                        AtVector locatorLocation2 = {lb.locatorLocation[i*3], lb.locatorLocation[i*3+1], lb.locatorLocation[i*3+2]};
-                        if (AiV3Dist(sg->P, locatorLocation2) < sphereRadius){
-                            inRadius = true;
-                        }
+                    for(int i = 0; i < lightblockers.lightFilterNames.size(); i++){
+                    AtVector locatorLocation2 = {lightblockers.locatorLocation[i*3], lightblockers.locatorLocation[i*3+1], lightblockers.locatorLocation[i*3+2]};
+                    if (AiV3Dist(sg->P, locatorLocation2) < sphereRadius){
+                        inRadius = true;
                     }
+                }
 
-                    if (inRadius != true){
-                        sg->Liu = AiColorLerp(inverseColorDensity, sg->Liu, sg->Liu * negativeColor);
-                    }
+                if (!inRadius){
+                    sg->Liu = AiColorLerp(colorDensity, sg->Liu, sg->Liu * colorAttenuation);
                 }
             }
         }
     }
+}
 
 }
+
 node_loader {
-                if (i > 0)
-                               return false;
-                node->methods     = lightColorAttenuationMethods;
-                node->output_type = AI_TYPE_RGB;
-                node->name        = "lightColorAttenuation";
-                node->node_type   = AI_NODE_SHADER;
-                strcpy(node->version, AI_VERSION);
-                return true;
+    if (i > 0){return false;}
+    node->methods     = lightColorAttenuationMethods;
+    node->output_type = AI_TYPE_RGB;
+    node->name        = "lightColorAttenuation";
+    node->node_type   = AI_NODE_SHADER;
+    strcpy(node->version, AI_VERSION);
+    return true;
 }
